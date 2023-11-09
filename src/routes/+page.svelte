092bbd72eed7fced1dc89bc6a20208385e20eb82
Wyatt Degenhart <wyattdegenhart@gmail.com>
2023-11-08 22:40:38 -0700

    routing changes and initial json structure

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,1 @@
-<script lang="ts">
-   import Test from "$lib/components/test.svelte"; 
-   
-   let question = "What is ____";
-   let answers = ["WAIUHOFAUIHWFUHIFWHUI", "WAFYGUAWFYGUFAWYUGFYGUAW", "AWYGUFAYGUFWYGUAFWYGU", "WGUAIFUIGAWFIUGHAFWIUGH"];
-</script>
-
-<Test />
+<h1>Hello</h1>
---
+++ src/routes/test/test.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+    let question = {
+        asks: "What is ___?",
+        answers: [
+            { id: 0, label: "AWIUAWFUHAWFUHIAW"},
+            { id: 1, label: "AIOFAQWIOFAWIFOHFI"},
+            { id: 2, label: "WUHFAFWUHFWFHU IA"},
+            { id: 3, label: "NVKJNAFNKWJAQFAWJKF"},
+        ]
+    }
+</script>
+
+<form>
+    <h1>{question.asks}</h1>
+    {#each question.answers as answer}
+        <input type="radio" name="answers" id={answer.id.toString()}/>
+        <label for={answer.id.toString()}>{answer.label}</label><br/>
+    {/each}
+    <input type="submit" />
+</form>
