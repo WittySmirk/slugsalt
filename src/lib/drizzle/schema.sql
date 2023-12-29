48a1d5e270bd9574748916879c17aff86b87b744
Wyatt Degenhart <wyattdegenhart@gmail.com>
2023-12-28 20:17:21 -0700

    Matched schema to schematic

---
+++ src/lib/drizzle/schema.sql
@@ -0,0 +1,35 @@
+CREATE TABLE user (
+    id TEXT NOT NULL PRIMARY KEY,
+    googleEmail TEXT NOT NULL,
+    currentQuestion TEXT,
+    correct INTEGER DEFAULT 0,
+    bottled INTEGER,
+    FOREIGN KEY (currentQuestion) REFERENCES question(id)
+);
+
+CREATE TABLE user_key (
+    id TEXT NOT NULL PRIMARY KEY,
+    user_id TEXT NOT NULL,
+    hashed_password TEXT,
+    FOREIGN KEY (user_id) REFERENCES user(id)
+);
+
+CREATE TABLE user_session (
+    id TEXT NOT NULL PRIMARY KEY,
+    user_id TEXT NOT NULL,
+    active_expires INTEGER NOT NULL,
+    idle_expires INTEGER NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES user(id)
+);
+
+CREATE TABLE white_list (
+    googleEmail TEXT NOT NULL,
+    bottled INTEGER NOT NULL
+);
+
+CREATE TABLE question (
+    id TEXT NOT NULL PRIMARY KEY,
+    question TEXT NOT NULL,
+    answers TEXT NOT NULL,
+    image TEXT
+);
--- src/lib/drizzle/schema.ts
+++ src/lib/drizzle/schema.ts
@@ -1,21 +1,35 @@
 import { relations, sql } from "drizzle-orm";
-import { blob, sqliteTable, text } from "drizzle-orm/sqlite-core";
+import { blob, int, sqliteTable, text } from "drizzle-orm/sqlite-core";
 
 export const user = sqliteTable('user', {
-    id: text('id', {length: 15}).primaryKey(),
-    username: text("username", {length:15}).notNull(),
-    googleEmail: text("googleEmail").notNull()
+    id: text('id').notNull().primaryKey(),
+    googleEmail: text('googleEmail').notNull(),
+    currentQuestion: text('currentQuestion').references(() => question.id),
+    correct: int('correct').default(0),
+    bottled: int('bottled')
 })
 
 const user_key = sqliteTable('user_key', {
-    id: text('id', {length: 255}).primaryKey(),
-    user_id: text('user_id', {length: 15}).notNull().references(() => user.id),
-    hashed_password: text('hashed_password', {length: 255}),
+    id: text('id').primaryKey(),
+    user_id: text('user_id',).notNull().references(() => user.id),
+    hashed_password: text('hashed_password'),
 })
 
 export const user_session = sqliteTable('user_session', {
-    id: text('id', {length: 127}).primaryKey(),
-    user_id: text('user_id', {length: 15}).notNull().references(() => user.id),
+    id: text('id').primaryKey(),
+    user_id: text('user_id').notNull().references(() => user.id),
     active_expires: blob('active_expires', { mode: "bigint"}).notNull(),
     idle_expires: blob('idle_expires', {mode: "bigint"}).notNull()
+})
+
+export const white_list = sqliteTable('white)list', {
+    googleEmail: text('googleEmail').notNull(),
+    bottled: int('bottled').notNull()
+})
+
+export const question = sqliteTable('question', {
+    id: text('id').notNull().primaryKey(),
+    question: text('question').notNull(),
+    answers: text('texts').notNull(),
+    image: text('image')
 })
